01fe994141f953d25de63a3d5eb7ee10e2946f8d
Eug-Chua <eugene.chua.mail@gmail.com>
2024-03-19 00:39:32 +0800

    Implemented CREATE and UPDATE portions

--- 06b-CRUD-Hands-On/index.js
+++ 06b-CRUD-Hands-On/index.js
@@ -3,8 +3,13 @@
 const express = require("express"); 
 const hbs = require("hbs");
 
-// create an express application
+// create an Express application
 const app = express();
+
+// enable for processing for Express
+app.use(express.urlencoded({
+    extended:false
+}))
 
 // setup hbs
 app.set('view engine', 'hbs');
@@ -15,21 +20,21 @@ const database = [
         "id":1,
         "title":"Preloved PS5",
         "price":800.00,
-        "payment":["cod", "paynow"],
+        "payments":["cod", "paynow"],
         "type":"entertainment"
     },
     {
         "id":2,
         "title":"IKEA Mirror",
         "price":30.00,
-        "payment":["paynow"],
+        "payments":["paynow"],
         "type":"household"
     },
     {
         "id":3,
         "title":"Samsung TV",
         "price":399.99,
-        "payment":["cod", "paynow"],
+        "payments":["cod", "paynow"],
         "type":"electronics"
     }
 ]
@@ -41,6 +46,35 @@ app.get("/", function(req,res){
     res.render("index", {
         'products':database
     });
+})
+
+app.get('/create-listing', function(req,res){
+    res.render('create-listing');
+})
+
+app.post('/create-listing', function(req,res){
+    console.log(req.body)
+    const title = req.body.title;
+    const price = req.body.price;
+    let payments = []
+    if (Array.isArray(req.body.payment)){
+        payments = req.body.payments;
+    } else if (req.body.payments) {
+        // if payments is inputted but isn't an array, set it as an array
+        payments = [req.body.payments];
+    }
+    const type = req.body.type;
+    const newProduct = {
+        "id": Math.floor(Math.random() * 10000 + 1),
+        "title":title,
+        "payments":payments,
+        "price":price,
+        "type":type
+    }
+    database.push(newProduct)
+    
+    // Instruct browser to go to a new URL
+    res.redirect("/")
 })
 
 // 3. START SERVER
